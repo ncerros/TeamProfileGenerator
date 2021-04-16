e7b5ef8f4f75508c04e38b8fa481eeff796f2faf
Norman Cerros <n_cerros@hotmail.com>
2021-04-15 19:28:27 -0600

    added a file

--- teamapp.js
+++ teamapp.js
@@ -1,7 +1,48 @@
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const chalk = require('chalk');
+
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+
+// Initial inquirer process to gather information about the development team members
+
+let teamArr = [];
+
+const startingQuestion = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Choose type of employee?',
+            name: 'employeeType',
+            choices: [Manager, Engineer, Intern]
+        },
+    ])
+        .then(answer => {
+
+            if (answer.employeeType === 'Manager') {
+                managerQuestions();
+            } else if
+                (answer.employeeType === 'Engineer') {
+                engineerQuestions();
+            } else if
+                (answer.employeeType === 'Intern') {
+                internQuestions();
+            }
+            else {
+                console.log('Done!');
+                return;
+            }
+        })
+}
+
+startingQuestion();
